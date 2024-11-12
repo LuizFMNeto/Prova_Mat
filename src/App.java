74792a0eeee27d6a715cdd4cbfe0ac8d1eaf8977
Luiz Neto <luizfariasmneto@gmail.com>
2024-11-12 15:21:29 -0300

    Trabalho finalizado

--- src/App.java
+++ src/App.java
@@ -1,29 +1,59 @@
 import java.util.Scanner;   
 
 public class App {
+    //Método para checar se a placa é válida segundo o padrão Mercosul (L L L N L N N)
+    static boolean checarPlacaPadronizada(String placaParam){
+        //String que contém o padrão a ser seguido e comparado
+        String padrao = "[A-Z]{3}[0-9][A-Z][0-9]{2}";
+
+        //Retorna 'TRUE' ou 'FALSE'
+        return placaParam.matches(padrao);
+    }
+
     public static void main(String[] args) {
         //Scanner
         Scanner scanner = new Scanner(System.in);
         
-        //Usuário informa a placa a ser consultada
-        System.out.println("Digite a placa do carro:");
-        String placaCompleta = scanner.nextLine().toUpperCase();
+        //Looping do sistema todo
+        while(true){
+            //Interface inicial do TERMINAL
+            System.out.println("-Deseja consultar uma placa?");
+            String resposta = scanner.nextLine().toUpperCase();
 
-        //Extrair apenas as 3 inicias da placa informada pelo usuário
-        String iniciasDaPlaca = placaCompleta.substring(0, 3);
+            //Checa a resposta do usuário
+            if(resposta.equals("SIM")){
+                //Usuário informa a placa a ser consultada
+                System.out.println("-Digite a placa do carro:");
+                String placaCompleta = scanner.nextLine().toUpperCase();
+                
+                //Looping para pegar uma placa válida segundo o padrão Mercosul
+                while(true){
+                    //Checar se a placa fornecida pelo usuário é válida
+                    if(checarPlacaPadronizada(placaCompleta)){
+                        System.out.println("--------------------------------------------");
+                        break;
+                    } else {
+                        System.out.println("--------------------------------------------");
+                        System.out.println("-A placa é inválida segundo o padão Mercosul.");
+                        System.out.println("-A placa deve ter padrão L L L N° L N° N°.");
+                        System.out.println("-Digite novamente a placa do carro:");
+                        placaCompleta = scanner.nextLine().toUpperCase();
+                    }
+                }
 
-        //Criar um objeto "Placa"
-        Placa placa1 = new Placa(placaCompleta , iniciasDaPlaca);
+                //Extrair apenas as 3 inicias da placa informada pelo usuário
+                String iniciasDaPlaca = placaCompleta.substring(0, 3);
 
-        //Checar se a placa fornecida pelo usuário é válida
-        if(placa1.checarPlacaPadronizada()){
-            System.out.println("Placa válida segundo Mercosul.");
-        } else {
-            System.out.println("A placa é inválida segundo o padão Mercosul.");
+                //Criar um objeto "Placa"
+                Placa placa1 = new Placa(placaCompleta , iniciasDaPlaca);
+        
+        
+                //Saída da resposta do sistema
+                System.out.println(placa1.checarLocalidadeDaPlaca(placaCompleta, iniciasDaPlaca));
+            } else {
+                System.out.println("Sistema encerrado.");
+                break;
+            }
         }
-
-        //Saída da resposta do sistema
-        System.out.println(placa1.checarLocalidadeDaPlaca(placaCompleta, iniciasDaPlaca));
-
     }
 }
--- src/Placa.java
+++ src/Placa.java
@@ -25,15 +25,6 @@ public class Placa {
         iniciasDaPlaca = novasIniciasDaPlaca;
     }
 
-    //Método para checar se a placa é válida segundo o padrão Mercosul (L L L N L N N)
-    public boolean checarPlacaPadronizada(){
-        //String que contém o padrão a ser seguido e comparado
-        String padrao = "[A-Z]{3}[0-9][A-Z][0-9]{2}";
-
-        //Retorna 'TRUE' ou 'FALSE'
-        return numeroDaPlaca.matches(padrao);
-    }
-
     //Método que checa de qual estado é  a placa fornecida pelo usuário
     public String checarLocalidadeDaPlaca(String numeroDaPlaca, String iniciasDaPlaca){
         if (iniciasDaPlaca.compareTo("HQF") >= 0 && iniciasDaPlaca.compareTo("HTW") <= 0) {
